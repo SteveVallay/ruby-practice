097f87803dd733ab56eaeac8c61f45144e4c191f
zhibinw <zhibinwang.q@gmail.com>
2013-08-01 14:12:25 +0800

    intro: hash

---
+++ intro/hash.rb
@@ -0,0 +1,12 @@
+inst_section = {
+  'cello'=>'strings',
+  'a' => 'a',
+  'b' => 'b',
+  'c' => 'c',
+  'd' => 'd',
+  'e' => 'e',
+}
+
+p 'a is ' + inst_section['a']
+p 'first is :' + inst_section['cello']
+p  inst_section['error']
