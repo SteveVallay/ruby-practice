ce19589f664c00864f6760b39bd65d30c4b89a73
zhibinw <zhibinwang.q@gmail.com>
2013-08-05 14:27:36 +0800

    access variables in classs.

--- chapter3/book.rb
+++ chapter3/book.rb
@@ -6,6 +6,12 @@ class BookInStock
   def to_s
     "ISBN:#{@isbn},price:#{@price}"
   end
+  def isbn
+    @isbn
+  end
+  def price
+    @price
+  end
 end
 
 
@@ -13,3 +19,6 @@ book = BookInStock.new("12345",123.231)
 
 p book
 puts book
+
+puts "ISBN: #{book.isbn}"
+puts "price: #{book.price}"
