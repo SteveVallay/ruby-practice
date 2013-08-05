130f954848195fd7bc7fc1387dc2a80949e4463f
zhibinw <zhibinwang.q@gmail.com>
2013-08-05 14:50:54 +0800

    attr_accessor attr_writer

--- chapter3/book.rb
+++ chapter3/book.rb
@@ -1,11 +1,15 @@
 class BookInStock
-  attr_reader :isbn,:price
+  attr_reader :isbn
+  attr_accessor:price
   def initialize(isbn,price)
     @isbn = isbn
     @price = Float(price)
   end
   def to_s
     "ISBN:#{@isbn},price:#{@price}"
+  end
+  def price=(new_price)
+    @price = new_price
   end
 end
 
@@ -16,4 +20,5 @@ p book
 puts book
 
 puts "ISBN: #{book.isbn}"
+book.price=154.123
 puts "price: #{book.price}"
