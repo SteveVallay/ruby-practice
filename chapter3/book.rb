1469f42aa687689b16f5e69919e6bc6df103fb73
zhibinw <zhibinwang.q@gmail.com>
2013-08-05 15:43:54 +0800

    virtual attributes

--- chapter3/book.rb
+++ chapter3/book.rb
@@ -8,7 +8,14 @@ class BookInStock
   def to_s
     "ISBN:#{@isbn},price:#{@price}"
   end
+  def price_in_cents
+    Integer(@price*100 + 0.5)
+  end
+  def price_in_cents=(cents)
+    @price = cents/100.0
+  end
 end
+
 
 
 book = BookInStock.new("12345",123.231)
@@ -19,3 +26,10 @@ puts book
 puts "ISBN: #{book.isbn}"
 book.price=154.123
 puts "price: #{book.price}"
+puts "price in cents #{book.price_in_cents}"
+
+book.price_in_cents=54213
+puts "price: #{book.price}"
+puts "price in cents #{book.price_in_cents}"
+
+
